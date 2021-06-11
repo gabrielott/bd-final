45a1166323229ba6df479bd31a99cf4b6b335494
gabrielott <gabrielott@icloud.com>
2021-06-11 00:07:10 -0300

    Interface de edição de questões

--- front/src/App.js
+++ front/src/App.js
@@ -400,6 +400,93 @@ function Question(props) {
 	);
 }
 
+class QuestionEditor extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			selected: 0,
+			questions: this.props.questions,
+		};
+
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleSelect = this.handleSelect.bind(this);
+	}
+
+	handleInputChange(event) {
+		const target = event.target;
+		const value = target.value;
+		const name = target.name;
+
+		const questions = this.state.questions.slice();
+		const question = questions[this.state.selected];
+		question[name] = value;
+
+		this.setState({questions: questions});
+
+		console.log(`index=${this.state.selected} name=${name} value=${value}`);
+	}
+
+	handleSelect(index) {
+		this.setState({selected: index});
+	}
+
+	render() {
+		const questions = this.props.questions.map((q, i) =>
+			<li
+				key={i}
+				className={this.state.selected === i ? "qeditor-question-selected" : "qeditor-question"}
+				onClick={() => this.handleSelect(i)}
+			>
+				{q.description}
+			</li>
+		);
+
+		const type_options = this.props.types.map((t, i) =>
+			<option value={i} key={i}>
+				{t.description}
+			</option>
+		);
+
+		const list_type_options = this.props.list_types.map((t, i) =>
+			<option value={i} key={i}>
+				{t.description}
+			</option>
+		);
+
+		return (
+			<div className="qeditor">
+				<div className="qeditor-left">
+					<ul>
+						{questions}
+					</ul>
+				</div>
+				<div className="qeditor-right">
+					<input
+						name="description"
+						value={this.state.questions[this.state.selected].description}
+						onChange={this.handleInputChange}
+					/>
+					<select
+						name="type"
+						value={this.state.questions[this.state.selected].type}
+						onChange={this.handleInputChange}
+					>
+						{type_options}
+					</select>
+					<select
+						name="list_type"
+						value={this.state.questions[this.state.selected].list_type}
+						onChange={this.handleInputChange}
+					>
+						{list_type_options}
+					</select>
+				</div>
+			</div>
+		);
+	}
+}
+
 function App(props) {
 	const types = [
 		{
@@ -431,22 +518,44 @@ function App(props) {
 		},
 	];
 
-	const question = {
-		description: "Uma pergunta",
-		type: 1,
-		list_type: 1,
-	}
+	const questions = [
+		{
+			description: "Uma pergunta",
+			type: 0,
+			list_type: 0,
+		},
+		{
+			description: "Outra pergunta",
+			type: 1,
+			list_type: 1,
+		},
+		{
+			description: "Mais uma pergunta",
+			type: 2,
+			list_type: 2,
+		},
+		{
+			description: "Quem diria? É outra pergunta",
+			type: 2,
+			list_type: 2,
+		},
+		{
+			description: "Outra",
+			type: 0,
+			list_type: 0,
+		},
+	];
 
 	const group = {
 		description: "Um grupo",
 		comment: "Comentando sobre o grupo",
-		questions: [question],
-	}
+		questions: questions,
+	};
 
 	const module = {
 		description: "Um módulo",
 		groups: [group],
-	}
+	};
 
 	return (
 		<Tabs className="tabs">
@@ -457,17 +566,19 @@ function App(props) {
 			</TabList>
 
 			<TabPanel>
-				<div className="survey">
-					<Survey
-						description="Title"
-						modules={[module]}
-						types={types}
-						list_types={list_types}
-					/>
-				</div>
+				<Survey
+					description="Title"
+					modules={[module]}
+					types={types}
+					list_types={list_types}
+				/>
 			</TabPanel>
 			<TabPanel>
-				<h1>Questões</h1>
+				<QuestionEditor
+					questions={questions}
+					types={types}
+					list_types={list_types}
+				/>
 			</TabPanel>
 			<TabPanel>
 				<h1>Listas</h1>
