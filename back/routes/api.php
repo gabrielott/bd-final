be0572cb07736a1a206f5d27bf11969b558fce94
filipecbs <filipecbs94@gmail.com>
2021-06-13 03:08:25 -0300

    + Rotas

--- back/routes/api.php
+++ back/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\QuestionnaireController;
 use App\Http\Controllers\ListTypeController;
+use App\Http\Controllers\QuestionTypeController;
+use App\Http\Controllers\ListOfValuesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +21,9 @@ use App\Http\Controllers\ListTypeController;
 
 Route::post('createUserAdmin', [UserController::class, 'createUserAdmin']);
 
+//Question
+
+
 // Questionnaire
 Route::post('createQuestionnaire', [QuestionnaireController::class, 'createQuestionnaire']);
 Route::get('listQuestionnaires', [QuestionnaireController::class, 'index']);
@@ -31,6 +36,14 @@ Route::delete('deleteQuestionnaire/{id}', [QuestionnaireController::class, 'dele
 Route::post('createListType', [ListTypeController::class, 'createListType']);
 Route::get('listListType', [ListTypeController::class, 'listListType']);
 Route::get('showListType/{id}', [ListTypeController::class, 'showListType']);
+Route::put('editItems/{id}', [ListTypeController::class, 'editItems']);
+Route::put('insertNewValue/{id}', [ListOfValuesController::class, 'insertNewValue']);
+
+//QuestionType
+Route::post('createQuestionType', [QuestionTypeController::class, 'createQuestionType']);
+Route::get('listQuestionType', [QuestionTypeController::class, 'index']);
+Route::get('showQuestionType/{id}', [QuestionTypeController::class, 'show']);
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
